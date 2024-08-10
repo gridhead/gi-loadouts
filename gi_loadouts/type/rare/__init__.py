7e4c29743d535ace32608b215a8365ea362a02f4
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2024-08-10 14:25:16 +0530

    Add pydantic dataclasses for rarities
    Co-authored-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>
    Co-authored-by: Avadhoot Dhere <avadhootd99@gmail.com>
    Co-authored-by: Prithviraj Chavan <prithvichavan56110@gmail.com>
    Co-authored-by: Shounak Dey <shounakdey@ymail.com>

---
+++ gi_loadouts/type/rare/__init__.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+__rare__ = {
+    "Star 1": 1,
+    "Star 2": 2,
+    "Star 3": 3,
+    "Star 4": 4,
+    "Star 5": 5,
+}
+
+
+Rare = Enum(
+    "Rare",
+    {
+        item.replace(" ", "_"): data for item, data in __rare__.items()
+    }
+)
