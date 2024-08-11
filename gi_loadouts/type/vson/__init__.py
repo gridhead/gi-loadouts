dc5df10708a261aa13fb75a1953446c95db6cf56
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2024-08-11 10:45:07 +0530

    Add vision types
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

---
+++ gi_loadouts/type/vson/__init__.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class Vision(str, Enum):
+    anemo = "Anemo"
+    cryo = "Cryo"
+    dendro = "Dendro"
+    electro = "Electro"
+    geo = "Geo"
+    hydro = "Hydro"
+    pyro = "Pyro"
+    none = "None"
