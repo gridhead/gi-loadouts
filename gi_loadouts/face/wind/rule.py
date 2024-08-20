fa2224a0c80d83f5948a1b9524ce7e034db9fcc5
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2024-08-20 22:29:04 +0530

    Refactor the final calculations codebase
    Co-authored-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>
    Co-authored-by: Dhruv Bhirud <bhiruddhruv@gmail.com>
    Co-authored-by: Koustabh Nag <koustabhplays@gmail.com>
    Co-authored-by: Shounak Dey <shounakdey@ymail.com>

--- gi_loadouts/face/wind/rule.py
+++ gi_loadouts/face/wind/rule.py
@@ -4,6 +4,7 @@ from gi_loadouts.data.arti import ArtiList
 from gi_loadouts.data.char import __charmaps__
 from gi_loadouts.data.weap import Family
 from gi_loadouts.face.util import truncate_text
+from gi_loadouts.face.wind.calc import Assess
 from gi_loadouts.face.wind.file import FileHandling
 from gi_loadouts.face.wind.wind import Ui_mainwind
 from gi_loadouts.type import arti
@@ -16,25 +17,23 @@ from gi_loadouts.type.arti.base import (
     MainStatType_SDOE,
     SecoStatType,
 )
-from gi_loadouts.type.calc import CHAR, TEAM, WEAP
 from gi_loadouts.type.char import CharName
 from gi_loadouts.type.char.cons import Cons
 from gi_loadouts.type.levl import Level
 from gi_loadouts.type.rare import Rare
-from gi_loadouts.type.stat import ATTR, STAT, __revmap__
 from gi_loadouts.type.vson import __visioncolour__
 from gi_loadouts.type.weap import WeaponStatType, WeaponType
 
 
-class Rule(QMainWindow, Ui_mainwind, FileHandling):
+class Rule(QMainWindow, Ui_mainwind, FileHandling, Assess):
     def __init__(self):
         super().__init__()
         self.collection = Collection()
         self.otptobjc = None
-        self.c_team = TEAM()
-        self.c_char = CHAR()
-        self.c_weap = WEAP()
-        self.c_tyvt = CHAR()
+        self.c_team = None
+        self.c_weap = None
+        self.c_tyvt = None
+        self.c_char = None
 
     def populate_dropdown(self):
         self.head_char_name.addItems([item.value for item in CharName])
@@ -205,123 +204,17 @@ class Rule(QMainWindow, Ui_mainwind, FileHandling):
         """
         TODO: Make into a separate function
         """
-        if self.head_char_name.currentText().strip() != "" and self.head_char_levl.currentText().strip() != "":
-            #self.otptobjc = OtptWindow(self.head_char_name.currentText())
-            #self.otptobjc.show()
-            char = __charmaps__[self.head_char_name.currentText()]()
-            char.levl = getattr(Level, self.head_char_levl.currentText().replace(" ", "_").replace("(", "").replace(")", "").replace("/", "_"))
-            self.c_char.attack = ATTR(stat_name=STAT.attack, stat_data=char.attack)
-            self.c_char.defense = ATTR(stat_name=STAT.defense, stat_data=char.defense)
-            self.c_char.health_points = ATTR(stat_name=STAT.health_points, stat_data=char.health_points)
-            prev_stat_data = getattr(self.c_char, self.c_char.revmap[char.seco.stat_name]).stat_data
-            curt_stat_data = char.seco.stat_data
-            # 100 + 26.8
-            setattr(self.c_char, self.c_char.revmap[char.seco.stat_name], ATTR(stat_name=char.seco.stat_name, stat_data=prev_stat_data + curt_stat_data))
-        """
-        TODO: Make into a separate function
-        """
-        if self.weap_area_type.currentText().strip() != "" and self.weap_area_name.currentText().strip() != "" and self.weap_area_refn.currentText().strip() != "" and self.weap_area_levl.currentText().strip() != "":
-            kind = self.weap_area_type.currentText().strip()
-            name = self.weap_area_name.currentText()
-            weap = Family[kind][name]
-            refn = self.weap_area_refn.currentIndex()
-            weap.levl = getattr(Level, self.weap_area_levl.currentText().replace(" ", "_").replace("(", "").replace(")", "").replace("/", "_"))
-            self.c_weap.base = ATTR(stat_name=WeaponStatType.attack.value, stat_data=weap.main_stat.stat_data)
-            if weap.seco_stat.stat_name != WeaponStatType.none:
-                self.c_weap.seco = ATTR(stat_name=weap.seco_stat_calc.stat_name.value, stat_data=weap.seco_stat_calc.stat_data)
-            if len(weap.refi_stat) != 0:
-                self.c_weap.refn = weap.refi_stat[refn]
-        """
-        TODO: Make into a separate function
-        """
-        for part in ["fwol", "pmod", "sdoe", "gboe", "ccol"]:
-            if getattr(self, f"arti_{part}_type").currentText().strip() != "":
-                for indx in ["main", "a", "b", "c", "d"]:
-                    if getattr(self, f"arti_{part}_name_{indx}").currentText().strip() != "":
-                        if getattr(self, f"arti_{part}_name_{indx}").currentText().strip() == "None":
-                            setattr(self.c_team, f"{part}_{indx}", ATTR(stat_name=STAT.none, stat_data=0.0))
-                        else:
-                            try:
-                                setattr(self.c_team, f"{part}_{indx}", ATTR(stat_name=__revmap__[getattr(self, f"arti_{part}_name_{indx}").currentText().strip()], stat_data=float(getattr(self, f"arti_{part}_data_{indx}").text().strip())))
-                            except ValueError:
-                                self.show_dialog(QMessageBox.Information, "Invalid input detected", "Please enter a valid input (eg. 69, 42.0 etc.)")
-        if self.collection.quad != "":
-            pack = getattr(ArtiList, self.collection.quad.replace(" ", "_").replace("'", "").replace("-", "_"))
-            self.c_team.pairdata_a = pack.value.pairdata
-            self.c_team.quaddata = pack.value.quaddata
-        elif self.collection.pair != []:
-            if len(self.collection.pair) == 1:
-                pack_a = getattr(ArtiList, self.collection.pair[0].replace(" ", "_").replace("'", "").replace("-", "_"))
-                self.c_team.pairdata_a = pack_a.value.pairdata
-            elif len(self.collection.pair) == 2:
-                pack_a = getattr(ArtiList, self.collection.pair[0].replace(" ", "_").replace("'", "").replace("-", "_"))
-                self.c_team.pairdata_a = pack_a.value.pairdata
-                pack_b = getattr(ArtiList, self.collection.pair[1].replace(" ", "_").replace("'", "").replace("-", "_"))
-                self.c_team.pairdata_b = pack_b.value.pairdata
-        print(self.c_team, "\n\n\n")
-        """
-        TODO: Make into a separate function
-        """
-        """
-        self.c_tyvt.attack = 0
-        self.c_tyvt.defense = 0
-        self.c_tyvt.health_points = 0
-        """
-        """
-        ARTIFACT
-        """
-        char_substats_data = getattr(self.c_char, self.c_char.revmap[char.seco.stat_name]).stat_data
-        setattr(self.c_tyvt, self.c_tyvt.revmap[char.seco.stat_name], ATTR(stat_name=char.seco.stat_name, stat_data=char_substats_data))
-        # c_tyvt = 126.8
-        for item in self.c_tyvt.revmap:
-            for kind in ["fwol", "pmod", "sdoe", "gboe", "ccol"]:
-                for indx in ["main", "a", "b", "c", "d"]:
-                    if getattr(self.c_team, f"{kind}_{indx}").stat_name == item:
-                        prev = getattr(self.c_tyvt, self.c_tyvt.revmap[item]).stat_data
-                        curt = getattr(self.c_team, f"{kind}_{indx}").stat_data
-                        setattr(self.c_tyvt, self.c_tyvt.revmap[item], ATTR(stat_name=item, stat_data=prev+curt))
-        """
-        SET BONUSES
-        """
-        for item in self.c_tyvt.revmap:
-            for indx in ["pairdata_a", "pairdata_b", "quaddata"]:
-                lict = getattr(self.c_team, indx)
-                if len(indx) != 0:
-                    for advn in lict:
-                        if advn.stat_name == item:
-                            prev = getattr(self.c_tyvt, self.c_tyvt.revmap[item]).stat_data
-                            curt = advn.stat_data
-                            setattr(self.c_tyvt, self.c_tyvt.revmap[item], ATTR(stat_name=item, stat_data=prev + curt))
-        """
-        WEAPON SUBSTAT
-        """
-        prev = getattr(self.c_tyvt, self.c_tyvt.revmap[self.c_weap.seco.stat_name]).stat_data
-        curt = self.c_weap.seco.stat_data
-        setattr(self.c_tyvt, self.c_tyvt.revmap[self.c_weap.seco.stat_name], ATTR(stat_name=self.c_weap.seco.stat_name, stat_data=prev + curt))
-        """
-        WEAPON REFINEMENT BONUSES
-        """
-        for item in self.c_tyvt.revmap:
-            for indx in self.c_weap.refn:
-                if item == indx.stat_name.value:
-                    prev = getattr(self.c_tyvt, self.c_tyvt.revmap[item]).stat_data
-                    curt = indx.stat_data
-                    setattr(self.c_tyvt, self.c_tyvt.revmap[item], ATTR(stat_name=item, stat_data=prev + curt))
-
-        self.c_tyvt.attack = ATTR(
-            stat_name=STAT.attack,
-            stat_data=(self.c_char.attack.stat_data + self.c_weap.base.stat_data) * (100 + self.c_tyvt.attack_perc.stat_data) / 100.0 + self.c_tyvt.attack.stat_data
-        )
-        self.c_tyvt.health_points = ATTR(
-            stat_name=STAT.health_points,
-            stat_data=self.c_char.health_points.stat_data * (100 + self.c_tyvt.health_points_perc.stat_data) / 100.0 + self.c_tyvt.health_points.stat_data
-        )
-        self.c_tyvt.defense = ATTR(
-            stat_name=STAT.defense,
-            stat_data=self.c_char.defense.stat_data * (100 + self.c_tyvt.defense_perc.stat_data) / 100.0 + self.c_tyvt.defense.stat_data
-        )
-
-        print(self.c_tyvt)
+        # self.otptobjc = OtptWindow(self.head_char_name.currentText())
+        # self.otptobjc.show()
+        try:
+            self.calc_stat()
+            print(self.c_tyvt)
+        except Exception:
+            self.show_dialog(
+                QMessageBox.Information,
+                "Invalid input detected",
+                "Please enter a valid input (eg. 69, 42.0 etc.)"
+            )
 
     def validate_lineedit_userdata(self, text):
         try:
