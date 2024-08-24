0046b0359487ec74ac10909c080c019cd38bd64f
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2024-08-24 09:40:18 +0530

    Rework assets management for the user interface
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- gi_loadouts/main.py
+++ gi_loadouts/main.py
@@ -4,16 +4,16 @@ import sys
 from PySide6.QtGui import QFontDatabase
 from PySide6.QtWidgets import QApplication
 
-from gi_loadouts.face import rsrc  # noqa
+from gi_loadouts.face import rsrc  # noqa: F401
 from gi_loadouts.face.wind.main import MainWindow
 
 
 def load_custom_font():
     fontlist = [
-        ":font/font/text/sans-bold.ttf",
-        ":font/font/text/sans-rlar.ttf",
-        ":font/font/text/sans-bdit.ttf",
-        ":font/font/text/sans-rlit.ttf",
+        ":text/font/text/sans-bold.ttf",
+        ":text/font/text/sans-rlar.ttf",
+        ":text/font/text/sans-bdit.ttf",
+        ":text/font/text/sans-rlit.ttf",
         ":icon/font/icon/icon-bold.ttf",
         ":icon/font/icon/icon-rlar.ttf",
     ]
