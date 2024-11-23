895dbfdadf862fdadb9f744f62679724338be6e9
Shounak Dey <shounakdey@ymail.com>
2024-11-23 21:15:03 +0530

    Update project and compatibility to v5.2 Phase 1
    Signed-off-by: Shounak Dey <shounakdey@ymail.com>

--- gi_loadouts/__init__.py
+++ gi_loadouts/__init__.py
@@ -3,8 +3,8 @@ from importlib.metadata import metadata
 __metadict__ = metadata("gi-loadouts").json
 __versdata__ = __metadict__.get("version")
 
-__gicompat_vers__ = "5.1"
-__gicompat_part__ = "2"
+__gicompat_vers__ = "5.2"
+__gicompat_part__ = "1"
 
 __donation__ = "https://github.com/sponsors/gridhead"
 __releases__ = "https://github.com/gridhead/gi-loadouts/releases"
