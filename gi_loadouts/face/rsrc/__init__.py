c0eac3f7fa105b07f5ccd90a2759782819adf7bb
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2024-12-21 22:11:16 +0530

    Remove unnecessary artwork references from the codebase
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- gi_loadouts/face/rsrc/__init__.py
+++ gi_loadouts/face/rsrc/__init__.py
@@ -7,7 +7,7 @@ from PySide6.QtCore import QFile, QIODevice
 
 from gi_loadouts import conf
 from gi_loadouts.face.rsrc import arti, data, pmon, rare, vson, weap  # noqa: F401
-from gi_loadouts.face.rsrc.char import back, cons, face, name, side, wish  # noqa: F401
+from gi_loadouts.face.rsrc.char import back, face, name, wish  # noqa: F401
 from gi_loadouts.face.rsrc.font import icon, text  # noqa: F401
 
 
