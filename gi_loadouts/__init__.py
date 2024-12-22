049fe0ac00a9c6c52051fb2fc42412926a65a863
Shounak Dey <shounakdey@ymail.com>
2024-12-22 18:57:09 +0530

    Correct the compatible version/phase
    Signed-off-by: Shounak Dey <shounakdey@ymail.com>

--- gi_loadouts/__init__.py
+++ gi_loadouts/__init__.py
@@ -4,7 +4,7 @@ __metadict__ = metadata("gi-loadouts").json
 __versdata__ = __metadict__.get("version")
 
 __gicompat_vers__ = "5.2"
-__gicompat_part__ = "1"
+__gicompat_part__ = "2"
 
 __donation__ = "https://github.com/sponsors/gridhead"
 __releases__ = "https://github.com/gridhead/gi-loadouts/releases"
