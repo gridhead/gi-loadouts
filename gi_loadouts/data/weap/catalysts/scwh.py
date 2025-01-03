fcb5e821929e53edef6b63681efabb49ee258f65
Shounak Dey <shounakdey@ymail.com>
2025-01-04 00:24:45 +0530

    Add missing refi_stat for Starcaller's Watch

--- gi_loadouts/data/weap/catalysts/scwh.py
+++ gi_loadouts/data/weap/catalysts/scwh.py
@@ -18,4 +18,11 @@ class StarcallersWatch(Catalyst):
         "Increases Elemental Mastery by 175. Gain the \"Mirror of Night\" effect within 15s after the equipping character creates a shield: The current active party member deals 49% increased DMG to nearby opponents. You can gain the \"Mirror of Night\" effect once every 14s.",
         "Increases Elemental Mastery by 200. Gain the \"Mirror of Night\" effect within 15s after the equipping character creates a shield: The current active party member deals 56% increased DMG to nearby opponents. You can gain the \"Mirror of Night\" effect once every 14s.",
     ]
+    refi_stat: List[WeaponStat] = [
+        [WeaponStat(stat_name=WeaponStatType.elemental_mastery, stat_data=100.0)],
+        [WeaponStat(stat_name=WeaponStatType.elemental_mastery, stat_data=125.0)],
+        [WeaponStat(stat_name=WeaponStatType.elemental_mastery, stat_data=150.0)],
+        [WeaponStat(stat_name=WeaponStatType.elemental_mastery, stat_data=175.0)],
+        [WeaponStat(stat_name=WeaponStatType.elemental_mastery, stat_data=200.0)],
+    ]
     file: str = "scwh"
