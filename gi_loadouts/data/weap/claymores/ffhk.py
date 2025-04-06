4bc28f026ca02da4d3351206fc6c9ffb3ba18caa
Shounak Dey <shounakdey@ymail.com>
2025-04-06 20:06:23 +0530

    Add codebase for new weapon `Fruitful Hook`
    Signed-off-by: Shounak Dey <shounakdey@ymail.com>

---
+++ gi_loadouts/data/weap/claymores/ffhk.py
@@ -0,0 +1,19 @@
+from gi_loadouts.type.rare import Rare
+from gi_loadouts.type.weap import Claymore, WeaponStat, WeaponStatType
+from gi_loadouts.type.weap.tier import Tier
+
+
+class FruitfulHook(Claymore):
+    name: str = "Fruitful Hook"
+    seco_stat: WeaponStat = WeaponStat(stat_name=WeaponStatType.attack_perc, stat_data=6.0)
+    tier: Tier = Tier.Tier_3
+    rare: Rare = Rare.Star_4
+    refi_name: str = "The Weight of Falling Branches"
+    refi_list: list[str] = [
+        "Increase Plunging Attack CRIT Rate by 16%; After a Plunging Attack hits an opponent, Normal, Charged, and Plunging Attack DMG increased by 16% for 10s.",
+        "Increase Plunging Attack CRIT Rate by 20%; After a Plunging Attack hits an opponent, Normal, Charged, and Plunging Attack DMG increased by 20% for 10s.",
+        "Increase Plunging Attack CRIT Rate by 24%; After a Plunging Attack hits an opponent, Normal, Charged, and Plunging Attack DMG increased by 24% for 10s.",
+        "Increase Plunging Attack CRIT Rate by 28%; After a Plunging Attack hits an opponent, Normal, Charged, and Plunging Attack DMG increased by 28% for 10s.",
+        "Increase Plunging Attack CRIT Rate by 32%; After a Plunging Attack hits an opponent, Normal, Charged, and Plunging Attack DMG increased by 32% for 10s.",
+    ]
+    file: str = "ffhk"
