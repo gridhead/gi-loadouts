106ee9361fe501956f5397a9fda0965f7288bae5
Shounak Dey <shounakdey@ymail.com>
2025-04-07 21:25:49 +0530

    Modify the file referencing from `png` to `webp`
    Signed-off-by: Shounak Dey <shounakdey@ymail.com>

--- gi_loadouts/face/info/main.py
+++ gi_loadouts/face/info/main.py
@@ -13,7 +13,7 @@ class InfoDialog(QDialog, Ui_info):
         super().__init__()
         self.setupUi(self)
         self.setWindowTitle(f"Loadouts for Genshin Impact v{__versdata__}")
-        self.icon.setPixmap(QPixmap(f":pmon/imgs/pmon/{int(time() % 10)}.png"))
+        self.icon.setPixmap(QPixmap(f":pmon/imgs/pmon/{int(time() % 10)}.webp"))
         self.vers.setText(f"Version v{__versdata__}")
         self.comp.setText(f"This version is compatible with Genshin Impact {__gicompat_vers__} Phase {__gicompat_part__}")
         self.updt.clicked.connect(self.open_update_link)
