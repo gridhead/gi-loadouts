d9c7148b64bb79b18a82a9c390cd4d8755c749c0
Shounak Dey <shounakdey@ymail.com>
2025-04-10 23:53:10 +0530

    Add chronological `TAB` order for the main window
     - List out all the widgets used in main window
     - Use `setTabOrder` to set the order of `TAB` for those widgets
    
    Signed-off-by: Shounak Dey <shounakdey@ymail.com>

---
+++ gi_loadouts/face/wind/__init__.py
@@ -0,0 +1,168 @@
+# List of widgets present in left sidebar
+sidebar_left_widget = [
+    "side_head",
+    "side_tckt",
+    "side_cash",
+    "side_lcns",
+    "side_info"
+]
+
+# List of widgets present in character information panel
+info_char_widget = [
+    "head_char_elem",
+    "head_char_name",
+    "head_char_cons",
+    "head_char_levl",
+    "head_char_data_attk",
+    "head_char_data_dfns",
+    "head_char_data_hlpt",
+    "head_char_data_subs",
+    "char_head_lumi",
+    "char_head_aeth"
+]
+
+# List of widgets present in artifact set information panel
+info_team_widget = [
+    "pair_area_desc",
+    "quad_area_desc",
+    "head_scan",
+    "head_load",
+    "head_save",
+    "head_wipe"
+]
+
+# List of widgets present in `Flower of Life` artifact information panel
+info_arti_fwol_widget = [
+    "arti_fwol_type",
+    "arti_fwol_levl",
+    "arti_fwol_rare",
+    "arti_fwol_name_main",
+    "arti_fwol_data_main",
+    "arti_fwol_name_a",
+    "arti_fwol_data_a",
+    "arti_fwol_name_b",
+    "arti_fwol_data_b",
+    "arti_fwol_name_c",
+    "arti_fwol_data_c",
+    "arti_fwol_name_d",
+    "arti_fwol_data_d",
+    "arti_fwol_scan",
+    "arti_fwol_load",
+    "arti_fwol_save",
+    "arti_fwol_wipe"
+]
+
+# List of widgets present in `Plume of Death` artifact information panel
+info_arti_pmod_widget = [
+    "arti_pmod_type",
+    "arti_pmod_levl",
+    "arti_pmod_rare",
+    "arti_pmod_name_main",
+    "arti_pmod_data_main",
+    "arti_pmod_name_a",
+    "arti_pmod_data_a",
+    "arti_pmod_name_b",
+    "arti_pmod_data_b",
+    "arti_pmod_name_c",
+    "arti_pmod_data_c",
+    "arti_pmod_name_d",
+    "arti_pmod_data_d",
+    "arti_pmod_scan",
+    "arti_pmod_load",
+    "arti_pmod_save",
+    "arti_pmod_wipe"
+]
+
+# List of widgets present in `Sands of Eon` artifact information panel
+info_arti_sdoe_widget = [
+    "arti_sdoe_type",
+    "arti_sdoe_levl",
+    "arti_sdoe_rare",
+    "arti_sdoe_name_main",
+    "arti_sdoe_data_main",
+    "arti_sdoe_name_a",
+    "arti_sdoe_data_a",
+    "arti_sdoe_name_b",
+    "arti_sdoe_data_b",
+    "arti_sdoe_name_c",
+    "arti_sdoe_data_c",
+    "arti_sdoe_name_d",
+    "arti_sdoe_data_d",
+    "arti_sdoe_scan",
+    "arti_sdoe_load",
+    "arti_sdoe_save",
+    "arti_sdoe_wipe"
+]
+
+# List of widgets present in `Goblet of Eonothem` artifact information panel
+info_arti_gboe_widget = [
+    "arti_gboe_type",
+    "arti_gboe_levl",
+    "arti_gboe_rare",
+    "arti_gboe_name_main",
+    "arti_gboe_data_main",
+    "arti_gboe_name_a",
+    "arti_gboe_data_a",
+    "arti_gboe_name_b",
+    "arti_gboe_data_b",
+    "arti_gboe_name_c",
+    "arti_gboe_data_c",
+    "arti_gboe_name_d",
+    "arti_gboe_data_d",
+    "arti_gboe_scan",
+    "arti_gboe_load",
+    "arti_gboe_save",
+    "arti_gboe_wipe"
+]
+
+# List of widgets present in `Circlet of Logos` artifact information panel
+info_arti_ccol_widget = [
+    "arti_ccol_type",
+    "arti_ccol_levl",
+    "arti_ccol_rare",
+    "arti_ccol_name_main",
+    "arti_ccol_data_main",
+    "arti_ccol_name_a",
+    "arti_ccol_data_a",
+    "arti_ccol_name_b",
+    "arti_ccol_data_b",
+    "arti_ccol_name_c",
+    "arti_ccol_data_c",
+    "arti_ccol_name_d",
+    "arti_ccol_data_d",
+    "arti_ccol_scan",
+    "arti_ccol_load",
+    "arti_ccol_save",
+    "arti_ccol_wipe"
+]
+
+# List of widgets present in weapon information panel
+info_weap_widget = [
+    "weap_area_type",
+    "weap_area_name",
+    "weap_area_refn",
+    "weap_area_levl",
+    "weap_area_batk",
+    "weap_area_stat",
+    "weap_head_load",
+    "weap_head_save"
+]
+
+# List of widgets present in weapon refinements information panel
+info_refn_widget = [
+    "weap_area_refn_body"
+]
+
+# List of unpacked widgets arranged as per chronological `TAB` order
+tab_order_wind = [
+    *sidebar_left_widget,
+    *info_char_widget,
+    *info_team_widget,
+    *info_arti_fwol_widget,
+    *info_arti_pmod_widget,
+    *info_arti_sdoe_widget,
+    *info_arti_gboe_widget,
+    *info_arti_ccol_widget,
+    *info_weap_widget,
+    *info_refn_widget
+]
